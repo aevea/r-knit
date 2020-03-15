3d774065c9342ab79c7bc30133f35401816e44f6
Simon Prochazka <simon@fallion.net>
2020-03-15 22:24:17 +0100

    chore: set up github module

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 use failure::Error;
-use graphql_client::*;
-use log::*;
 use prettytable::*;
 use serde::*;
-
-type URI = String;
-type DateTime = String;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "src/schema/schema.public.graphql",
-    query_path = "src/queries/repository.graphql",
-    response_derives = "Debug"
-)]
-struct RepoView;
+mod github;
 
 #[derive(Deserialize, Debug)]
 struct Env {
@@ -26,49 +14,14 @@ fn main() -> Result<(), Error> {
 
     let config: Env = envy::from_env()?;
 
-    let query = RepoView::build_query(repo_view::Variables {
-        owner: "outillage".to_string(),
-        name: "commitsar".to_string(),
-    });
-
-    let client = reqwest::Client::new();
-
-    let mut res = client
-        .post("https://api.github.com/graphql")
-        .bearer_auth(config.github_api_token)
-        .json(&query)
-        .send()?;
-
-    let response_body: Response<repo_view::ResponseData> = res.json()?;
-    info!("{:?}", response_body);
-
-    if let Some(errors) = response_body.errors {
-        println!("there are errors:");
-
-        for error in &errors {
-            println!("{:?}", error);
-        }
-    }
-
-    let response_data: repo_view::ResponseData = response_body.data.expect("missing response data");
-
-    let stars: Option<i64> = response_data
-        .repository
-        .as_ref()
-        .map(|repo| repo.stargazers.total_count);
-
-    println!(
-        "{}/{} - 🌟 {}",
-        "outillage",
-        "commitsar",
-        stars.unwrap_or(0),
-    );
+    github::say_hi();
+    let pull_requests = github::pull_requests::get(config.github_api_token)?;
 
     let mut table = prettytable::Table::new();
 
     table.add_row(row!(b => "issue", "opened_at", "closed_at"));
 
-    for issue in &response_data
+    for issue in &pull_requests
         .repository
         .expect("missing repository")
         .pull_requests
